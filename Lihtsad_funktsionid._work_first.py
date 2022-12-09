6b8c246a201c32bd70b385aeafa5d488f794d934
opilane <opilane@HP-CZC6297B9M>
2022-12-09 13:50:51 +0200

    Add project files.

---
+++ Lihtsad_funktsionid._work_first.py
@@ -0,0 +1,4 @@
+print("Tere tulemast") #info ekraanile, väljundlause
+nimi=input("Mis sinu nimi on?") #Sisend lugemine jaoks, input()->str
+print() #tühi rida
+print(f"{nimi}, sul on väga ilus nimi") 
